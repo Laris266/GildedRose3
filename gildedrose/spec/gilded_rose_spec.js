8a6d888bf4f5932503a9578395c6b22c490edd76
Laris266 <if25b266@technikum-wien.at>
2026-06-24 15:03:50 +0200

    Test2 green for quality decreases twice as fast if sell by date has passed

--- gildedrose/spec/gilded_rose_spec.js
+++ gildedrose/spec/gilded_rose_spec.js
@@ -6,7 +6,7 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual("fixme");
   }); */
 
-  it("normal item decreases quality and sellIn by 1"), () =>
+  it("normal item decreases quality and sellIn by 1", () =>
   {
     const item = new Item("+5 Dexterity Vest", 10, 20);
 
@@ -15,6 +15,16 @@ describe("Gilded Rose", function() {
 
     expect(item.sell_in).toBe(9);
     expect(item.quality).toBe(19);
-  }
+  });
+
+  it("if sell by date has passed, quality degrades twice as fast", () => {
+    const item = new Item("+5 Dexterity Vest", 0, 20);
+
+    items = [item];
+    update_quality();
+
+    expect(item.sell_in).toBe(-1);
+    expect(item.quality).toBe(18);
+  });
 
 });
