9ea7577aacc61c6998a21d6345789524a7f4ed37
Laris266 <if25b266@technikum-wien.at>
2026-06-24 14:55:04 +0200

    Test1 green for normal item quality -1 sellIn -1

--- gildedrose/spec/gilded_rose_spec.js
+++ gildedrose/spec/gilded_rose_spec.js
@@ -1,9 +1,20 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
+ /*  it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
     expect(items[0].name).toEqual("fixme");
-  });
+  }); */
+
+  it("normal item decreases quality and sellIn by 1"), () =>
+  {
+    const item = new Item("+5 Dexterity Vest", 10, 20);
+
+    items = [item];
+    update_quality();
+
+    expect(item.sell_in).toBe(9);
+    expect(item.quality).toBe(19);
+  }
 
 });
